55f922bba05b73cb8de0af4db86c3d860fe90c7b
sreenandini <mallelasreenandini2@gmail.com>
2019-10-09 06:29:11 +0100

    added ero curency converter

--- src/app.js
+++ src/app.js
@@ -2,8 +2,28 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
-    data: { // NEW
-
+    data: {
+      rates:[],
+      startCurrency:"",
+      endCurrency:0,
+      beforeconvertion:0,
+      afterconvertion:0,
+    },
+    mounted(){
+        this.fetchRates()
+      },
+    computed: {
+        convertFromEuros: function() {
+          const euroConversion = this.startCurrency*this.beforeconvertion
+           return euroConversion.toFixed(2)
+      },
+    },
+    methods:{
+      fetchRates: function(){
+        const request = fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(data => this.rates = data.rates);
+      },
     }
   });
 });
